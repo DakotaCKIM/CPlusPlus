d663d83c48631cbda182f7001e06032ceb744438
Dakota Kim <dakotackim@gmail.com>
2014-05-22 01:20:23 -0400

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,4 +6,5 @@ main()
         std::cout << "My name is Dakota! What's yours?\n";
         std::cout << "That's a cool name...I wasn't really asking...\n";
         std::cout << "I'm having vegan burgers.\n";
+        std::cout << "Oops. I didn't commit anything today. Guess this works."
     }
